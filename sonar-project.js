0e6731ea4f4e620e33f9e8f55cb506d4a490e086
Fred <fokwunna@gmail.com>
2021-12-07 20:54:56 -0600

    Added more files to repo

---
+++ sonar-project.js
@@ -0,0 +1,18 @@
+const sonarqubeScanner = require('sonarqube-scanner');
+
+sonarqubeScanner({
+  serverUrl: 'http://172.31.30.243:9000/',
+       options : {
+	    'sonar.projectDescription': 'This is a Node JS application',
+	    'sonar.projectName': 'Node JS Application - Sample',
+	    'sonar.projectKey':'NodeJsLandmarkTechnologies',
+	    'sonar.login': '49d6f5bc1b6aad8034f68d9bca8441c73d97a55b',
+	    //'sonar.password': 'admin',
+            'sonar.projectVersion':'1.0',
+	    'sonar.language':'js',
+            'sonar.sourceEncoding':'UTF-8',
+            'sonar.sources': '.',
+	  //'sonar.tests': 'specs',
+          //'sonar.inclusions' : 'src/**'
+       },
+}, () => {});
